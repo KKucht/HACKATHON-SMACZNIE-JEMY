4a600043fd317cb7b315e6e5e92373a1919f62cc
borsuczek <meles.aleksandra@gmail.com>
2023-01-12 22:05:13 +0100

    changed once again

--- minutes_from_coordinates.py
+++ minutes_from_coordinates.py
@@ -1,6 +1,12 @@
 import geopy.distance
 
-def minutes_from_coordinates(coords1, coords2, v):
+avg_speeds = {
+    'walk' : 1.4,
+    'bike' : 57.6
+}
+
+def minutes_from_coordinates(coords1, coords2, mode):
+    speed = avg_speeds[mode] * 3,6
     s = geopy.distance.geodesic((coords1[1], coords1[0]), (coords2[1], coords2[0])).km
     min_in_h = 60
-    return s/v*min_in_h
+    return s/speed*min_in_h
