c9f132ceb219f0be11914c811e8dfe7c903fe216
borsuczek <meles.aleksandra@gmail.com>
2023-01-12 21:35:38 +0100

    changed function

--- minutes_from_coordinates.py
+++ minutes_from_coordinates.py
@@ -1,6 +1,6 @@
 import geopy.distance
 
 def minutes_from_coordinates(coords1, coords2, v):
-    s = geopy.distance.geodesic(coords1, coords2).km
+    s = geopy.distance.geodesic((coords1[1], coords1[0]), (coords2[1], coords2[0])).km
     min_in_h = 60
     return s/v*min_in_h
