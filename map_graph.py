b55442b9cd01460aa7d143ea948da957cc05dc76
PLangowski <pawell092000@gmail.com>
2023-01-12 21:08:20 +0100

    added a little tiny comment

--- map_graph.py
+++ map_graph.py
@@ -3,5 +3,7 @@ import networkx as nx
 import folium
 
 def get_map_graph(mode):
+    # mode: 'walk' or 'bike
+    
     place = 'Gdańsk, Poland'
     return ox.graph_from_place(place, network_type=mode)
