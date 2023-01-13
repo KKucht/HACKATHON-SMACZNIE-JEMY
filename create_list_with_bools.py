cc91eefc2108662444618d975bf5bcbe5170d8cc
borsuczek <meles.aleksandra@gmail.com>
2023-01-13 04:08:11 +0100

    changed create_list_with_bools

--- create_list_with_bools.py
+++ create_list_with_bools.py
@@ -6,18 +6,21 @@ def create_list_with_bools(dic_locations,point, graph_for_walking, graph_for_bik
     new_dict = {}
     for key in dic_locations.keys():
         locations = dic_locations[key]
-        new_dict[key] = False
-        for location in locations:
+        closest = [False, (0, 0, 'xd'), 'empty']
+        closest_time = 10000
+        for long_location in locations:
+            location = (long_location[0], long_location[1])
             if minutes_from_coordinates(point, location, 'bike') <= 15.0:
-                print("1\n")
-                if time_between_points(graph_for_biking,point, location, 'bike') <= 15.0:
-                    new_dict[key] = True
-                    break
-                print("2\n")
                 if minutes_from_coordinates(point, location, 'walk') <= 15.0:
-                    print("3\n")
-                    if time_between_points(graph_for_walking,point, location, 'walk') <= 15.0:
-                        new_dict[key] = True
-                        break
-                    print("4\n")
+                    time = time_between_points(graph_for_walking,point, location, 'walk')
+                    if time <= 15.0:
+                        if closest[2] == 'bike' or time < closest_time:
+                            closest_time = time
+                            closest = [True, long_location, 'walk']
+                if closest[2] != 'walk':
+                    bike_time = time_between_points(graph_for_biking,point, location, 'bike')
+                    if bike_time <= 15.0 and bike_time < closest_time:
+                        closest_time = bike_time
+                        closest = [True, long_location, 'bike']
+        new_dict[key] = closest
     return new_dict
